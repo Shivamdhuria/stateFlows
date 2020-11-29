e5e979219502a6744b6b7134d5ee3f0699cb997c
Shivam Dhuria <sdhuria@ask-fast.com>
2020-11-30 01:35:33 +0530

    fixed time

--- app/src/main/java/com/example/stateflow/debounce/DebounceViewmodel.kt
+++ app/src/main/java/com/example/stateflow/debounce/DebounceViewmodel.kt
@@ -1,11 +1,15 @@
 package com.example.stateflow.debounce
 
+import android.util.Log
 import androidx.annotation.AnyThread
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.withContext
 
 @ExperimentalCoroutinesApi
@@ -18,13 +22,18 @@ class DebounceViewmodel : ViewModel() {
     }
 
     val networkOperationResult: Flow<String> = _searchQuery.debounce(1000).mapLatest {
-        return@mapLatest longNetworkOperation(it)
+        if (it.isEmpty()) {
+            return@mapLatest ""
+        } else {
+            Log.e("Latest Mapped String Query", it)
+            return@mapLatest longNetworkOperation(it)
+        }
     }
 
     @AnyThread
     private suspend fun longNetworkOperation(request: String): String =
         withContext(Dispatchers.Default) {
-            delay(3000)
+            delay(1000)
             "Pseudo network delay of 3s delay: $request"
         }
 }
